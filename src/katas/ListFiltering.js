fa7c334b8597431d270e759247d76b8723b8d8fd
fdavison1 <fred.noreply@gmail.com>
2019-12-11 16:06:58 -0700

    sort by last char

--- src/katas/ListFiltering.js
+++ src/katas/ListFiltering.js
@@ -1,13 +1,42 @@
-let arr = [1, 2, 'a']
+//LIST FILTERING
 
-function filter_list(arr){
-    const newList = []
-    arr.map( item => {
-        if (typeof item === 'number'){
-            newList.push(item)
-        }
-    })
-    return newList
-}
+// let arr = [1, 2, 'a']
+
+// function filter_list(arr){
+//     const newList = []
+//     arr.map( item => {
+//         if (typeof item === 'number'){
+//             newList.push(item)
+//         }
+//     })
+//     return newList
+// }
 
 // filter_list(arr)
+
+//SORT BY LAST CHAR
+let str = 'what time are we climbing up the volcano'
+
+function last(x){
+    let arr = []
+    let arr2 = []
+    let test = x.split(' ')
+    for (let i = 0; i < test.length; i++){
+        let lastLetter = test[i][test[i].length-1]
+
+        let index = lastLetter.charCodeAt(0)
+
+        arr.push(lastLetter + i + test[i])
+    }
+    arr.sort()
+    for (let i =0; i <arr.length; i++){
+        let letters = arr[i].split('')
+        letters.shift()
+        letters.shift()
+        let word = letters.join('')
+        arr2.push(word)
+    }
+    return arr2
+}
+
+console.log(last(str))
