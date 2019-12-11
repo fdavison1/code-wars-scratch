f108edc81281cc0276021733683ef5fb93e0fa94
fdavison1 <fred.noreply@gmail.com>
2019-12-11 16:27:46 -0700

    printer error

---
+++ src/katas/LastChar.js
@@ -0,0 +1,24 @@
+//SORT BY LAST CHAR
+let str = 'what time are we climbing up the volcano'
+
+function last(x){
+    let arr = []
+    let arr2 = []
+    let test = x.split(' ')
+    for (let i = 0; i < test.length; i++){
+        let lastLetter = test[i][test[i].length-1]
+
+        arr.push(lastLetter + i + test[i])
+    }
+    arr.sort()
+    for (let i =0; i <arr.length; i++){
+        let letters = arr[i].split('')
+        letters.shift()
+        letters.shift()
+        let word = letters.join('')
+        arr2.push(word)
+    }
+    return arr2
+}
+
+console.log(last(str))
--- src/katas/ListFiltering.js
+++ src/katas/ListFiltering.js
@@ -14,29 +14,4 @@
 
 // filter_list(arr)
 
-//SORT BY LAST CHAR
-let str = 'what time are we climbing up the volcano'
 
-function last(x){
-    let arr = []
-    let arr2 = []
-    let test = x.split(' ')
-    for (let i = 0; i < test.length; i++){
-        let lastLetter = test[i][test[i].length-1]
-
-        let index = lastLetter.charCodeAt(0)
-
-        arr.push(lastLetter + i + test[i])
-    }
-    arr.sort()
-    for (let i =0; i <arr.length; i++){
-        let letters = arr[i].split('')
-        letters.shift()
-        letters.shift()
-        let word = letters.join('')
-        arr2.push(word)
-    }
-    return arr2
-}
-
-console.log(last(str))
