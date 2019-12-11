a56ac8ec997eb4b59320d9290062e5433634ce4c
fdavison1 <fred.noreply@gmail.com>
2019-12-11 13:08:04 -0700

    list_filter

---
+++ src/katas/ListFiltering.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 'a']
+
+function filter_list(arr){
+    const newList = []
+    arr.map( item => {
+        if (typeof item === 'number'){
+            newList.push(item)
+        }
+    })
+    return newList
+}
+
+// filter_list(arr)
