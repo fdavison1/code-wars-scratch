d591a28367fef769706a30a68dc171989c6e1115
fdavison1 <fred.noreply@gmail.com>
2019-12-13 09:54:22 -0700

    friend or foe

--- src/katas/FriendOrFoe.js
+++ src/katas/FriendOrFoe.js
@@ -1,3 +1,12 @@
+let input = ["Ryan", "Kieran", "Jason", "Yous"]
+
 function friend(friends){
     //your code here
+    let arr = []
+    for (let i=0; i < friends.length; i++){
+        if (friends[i].length === 4){
+            arr.push(friend[i])
+        }
+    }
+    return arr
   }
